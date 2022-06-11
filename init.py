92ee5af877b67e9fae6a262985b974c2fd2f8127
kensoi <65781959+kensoi@users.noreply.github.com>
2022-06-11 18:39:24 +0500

    Build 007

--- init.py
+++ init.py
@@ -1,5 +1,13 @@
-import engine
-engine.init(
-    "fead8551c4e6b575ebd39dc6e347c0a660afbd91041ff47041ddcbc0d2edce3858efcfeb58b62fd63de1c", 
-    "195675828")
+import importlib
+
+
+i = input('\n\n\n\u0009Write a version: ')
+token = "fead8551c4e6b575ebd39dc6e347c0a660afbd91041ff47041ddcbc0d2edce3858efcfeb58b62fd63de1c"
+identificator = "195675828"
+while i != 'exit':
+    engine = importlib.import_module('engine_'+i)
+    importlib.reload(engine)
+    engine.init(token, identificator) #старый движок, и типа ах ах кончи
+    engine = ''
+    i = input('\n\u0009Write a type: ')
 
